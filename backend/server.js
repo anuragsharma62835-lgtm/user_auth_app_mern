e7a4c28fb9309b65501160532aa9568ca91fedca
Anurag Sharma <anuragsharma62835@gmail.com>
2026-02-26 15:06:29 +0530

    fix bugs

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5173",
       process.env.CLIENT_URL,
     ],
+    credentials: true,
   })
 );
 
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://user-auth-app-mern.onrender.com/api"
+  baseURL: "https://user-auth-app-mern.onrender.com/api",
+   withCredentials: true,
 });
 
 export default API;
