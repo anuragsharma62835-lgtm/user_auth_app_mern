90d273d4d63129f2862b667b9046b1347454aae5
Anurag Sharma <anuragsharma62835@gmail.com>
2026-02-26 13:01:48 +0530

    fix casing

--- backend/middleware/ratelimiter.js
+++ backend/middleware/ratelimiter.js
@@ -1,7 +1,7 @@
 const ratelimit = require("express-rate-limit");
 
 const ratelimiter = ratelimit({
-  max: 2000,
+  max: 10,
   windowMs: 60 * 60 * 1000,
   message: "too many requests from this IP",
 });
--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const expressRateLimiter = require("express-rate-limiter");
-const ratelimiter = require("./middleware/ratelimiter");
+
+// custom rate limiter middleware
+const rateLimiter = require("./middleware/rateLimiter");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", ratelimiter,require("./routes/authRoutes"));
-app.use("/api/user", ratelimiter,require("./routes/userroutes"));
+// Routes
+app.use("/api/auth", rateLimiter, require("./routes/authroutes"));
+app.use("/api/user", rateLimiter, require("./routes/userroutes")); 
 
-
+// Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
